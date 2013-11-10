2caa862d4a9d93f9d17e59586620c61ae5ed3441
HidenGitHubRepo <u8q18@abv.bg>
2013-11-10 07:27:38 +0200

    .NET Framework 4.5 !!!!!!!!!!!!!!!!!!!!!!!!
    .NET Framework 4.5.NET Framework 4.5.NET Framework 4.5.NET Framework
    4.5.NET Framework 4.5.NET Framework 4.5.NET Framework 4.5.NET Framework
    4.5

---
+++ PrivateDistributor/CodeFirst.Model/User.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst.Model
+{
+    public class User
+    {
+        public User()
+        {
+            this.Cars = new HashSet<Car>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
+        [StringLength(30)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
+        [StringLength(30)]
+        public string DisplayName { get; set; }
+
+        [MinLength(40)]
+        [MaxLength(40)]
+        [StringLength(40)]
+        public string AuthCode { get; set; }
+
+        [MinLength(50)]
+        [MaxLength(50)]
+        [StringLength(50)]
+        public string SessionKey { get; set; }
+
+        public UserType UserType { get; set; }
+
+        public string Mail { get; set; }
+        public string АdditionalMail { get; set; }
+        
+        public string Phone { get; set; }
+        public string АdditionalPhone { get; set; }
+
+        public string Location { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; } 
+    } 
+}
---
+++ PrivateDistributor/PrivateDistributor.Data/DistributorContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirst.Model;
+
+namespace PrivateDistributor.Data
+{
+    public class DistributorContext : DbContext
+    {
+        public DistributorContext()
+            : base("DistributorDb")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
